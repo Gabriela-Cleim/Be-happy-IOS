849a004c9f673d17fa71c2a53edd608afe412f66
Gabriela Cleim <gabriela.cleims@gmail.com>
2022-02-19 21:04:38 -0300

    hihi

---
+++ Be happy/viewController/abas/ChatBot.swift
@@ -0,0 +1,8 @@
+//
+//  ChatBot.swift
+//  Be happy
+//
+//  Created by Gabriela Cleim on 19/02/22.
+//
+
+import Foundation
---
+++ Be happy/viewController/abas/ChatBotTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ChatBotTableViewCell.swift
+//  Be happy
+//
+//  Created by Gabriela Cleim on 19/02/22.
+//
+
+import UIKit
+
+class ChatBotTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
